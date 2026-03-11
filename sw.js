1e03a19292a6963c8cefd75730cb70fe2638c970
maintenancemalamnutech-ux <maintenancemalam.nutech@gmail.com>
2026-03-11 16:00:57 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'maintenancepro-v1';
+const FILES = [
+  '/Maintenancepro/',
+  '/Maintenancepro/index.html',
+  '/Maintenancepro/maintenance-login.html',
+  '/Maintenancepro/dashboard.html',
+  '/Maintenancepro/jadwal.html',
+  '/Maintenancepro/users.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
